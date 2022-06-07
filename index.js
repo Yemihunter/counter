5806cf6a34f2f01a10694e594c0f6b3e8aa804d7
Yemihunter <103632354+Yemihunter@users.noreply.github.com>
2022-06-07 11:12:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("plus1").addEventListener("click", function() {
+  document.getElementById("output").stepUp();
+});
+document.getElementById("minus1").addEventListener("click", function() {
+  document.getElementById("output").stepDown();
+});
